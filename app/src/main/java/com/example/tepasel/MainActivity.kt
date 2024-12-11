7b09f628d5b8ae16c6a376539eb672ce327e722a
Hamidreza shadabkia <h.shadabikia@dariahamrah.ir>
2024-12-11 15:23:53 +0330

    add item_install xml

--- app/src/main/java/com/example/tepasel/MainActivity.kt
+++ app/src/main/java/com/example/tepasel/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.tepasel
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ir.tapsell.plus.TapsellPlus
+import ir.tapsell.plus.TapsellPlusInitListener
+import ir.tapsell.plus.model.AdNetworkError
+import ir.tapsell.plus.model.AdNetworks
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val key = "lhdghcmffbqqtiffaigbcfpfpcgfqbsnleoltfadsqpolocsrgqhfnfcegqlfnejgiitfi"
+
+        TapsellPlus.initialize(this, key,
+            object : TapsellPlusInitListener {
+                override fun onInitializeSuccess(p0: AdNetworks?) {
+                    Log.d("onInitializeSuccess", p0?.name ?: "");
+                }
+
+                override fun onInitializeFailed(p0: AdNetworks?, p1: AdNetworkError?) {
+                    Log.d("onInitializeSuccess", p0?.name ?: "");
+                }
+
+            })
+
+        TapsellPlus.setDebugMode(Log.DEBUG)
+
     }
 }
